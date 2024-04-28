bc131a83cbd05f3ed1ac64db6926cf4162fe6848
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-04-27 22:22:50 -0400

    hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
 import Title from "@/components/title";
+import Image from "next/image";
+import Knotbin from "./knotbin.svg";
 
 export default function Home() {
   return (
-  	<div class="container">
-		<Title></Title>
-	</div>
+    <div class="container">
+      <div className="heroText">
+        <Title></Title>
+        <p>
+          An iOS engineer making open-source software for people, not
+          algorithms.
+        </p>
+      </div>
+      <Image src={Knotbin} className="icon" />
+    </div>
   );
 }
