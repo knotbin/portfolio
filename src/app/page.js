4a99ae2eda264b8a2c1a76324081fcc6b2c2f266
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-04-30 12:09:53 -0400

    remove tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
 import Title from "@/components/title";
 import Image from "next/image";
 import Knotbin from "./knotbin.svg";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div class="container">
-      <div className="heroText">
+    <div class={styles.container}>
+      <div className={styles.heroText}>
         <Title></Title>
       </div>
-      <Image src={Knotbin} className="icon" />
+      <Image src={Knotbin} className={styles.icon} />
     </div>
   );
 }
