1b17c1ee67747142b43dce80e2941d867a5a1531
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-05-05 22:46:36 -0400

    file name for warning

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,19 @@ import Title from "@/components/title";
 import Image from "next/image";
 import KnotbinText from "./KnotbinText.svg";
 import styles from "./page.module.css";
+import Acheivements from "@/components/acheivements";
 
 export default function Home() {
   return (
     <div>
-      <div class={styles.container}>
+      <div class="container">
         <Image src={KnotbinText} className={styles.titleicon}></Image>
       </div>
-      <div class={styles.sidecontainer}>
+      <div class="sidecontainer">
         <Title></Title>
+      </div>
+      <div class="container">
+        <Acheivements></Acheivements>
       </div>
     </div>
   );
--- src/components/acheivements.js
+++ src/components/acheivements.js
@@ -1,7 +1,7 @@
-export default Acheivements() {
+export default function Acheivements() {
   return(
     <div>
-
+      <h1 className="meshed">Winner of Apple Swift Student Challenge 2024</h1>
     </div>
   )
 }
