55dc4d74875f0365d2d00f3f550c1613acf60c6f
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-05-09 17:43:14 -0400

    socials added

--- src/components/socials.js
+++ src/components/socials.js
@@ -1,8 +1,21 @@
+import Image from 'next/image'
+import Twitter from '../media/twitter.svg'
+import Bluesky from '../media/bluesky.svg'
+import Github from '../media/github.svg'
+import Threads from '../media/threads.svg'
+import Signal from '../media/signal.svg'
+
 export default function Socials() {
     return (
         <div style={{ padding:70 }} className="container">
-            <h2 className="lemon">LET'S CONNECT!</h2>
-            
+            <h2 className="lemon">LET'S CONNECT</h2>
+            <div className="hocontainer">
+                <a style={{ margin:20 }} href='https://twitter.com/knotbin' target='_blank'><Image src={Twitter} width={55} height={55}></Image></a>
+                <a style={{ margin:20 }} href='https://bsky.app/profile/knotbin.xyz' target='_blank'><Image src={Bluesky} width={55} height={55}></Image></a>
+                <a style={{ margin:20 }} href='https://github.com/knotbin' target='_blank'><Image src={Github} width={55} height={55}></Image></a>
+                <a style={{ margin:20 }} href='https://threads.net/@knotbin' target='_blank'><Image src={Threads} width={55} height={55}></Image></a>
+                <a style={{ margin:20 }} href='https://signal.me/#eu/6dL59nT5AXrfuctsT4IT81iP5SPvaYbz_glV5XaTtaKDfuDcwOREKa02KyPVgk6Y' target='_blank'><Image src={Signal} width={55} height={55}></Image></a>
+            </div>
         </div>
     )
 }
