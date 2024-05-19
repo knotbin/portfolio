9f660c162bcdc50e04fc8b1c8131b3c067844324
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-05-18 23:58:09 -0400

    change css nopad

--- src/components/socials.js
+++ src/components/socials.js
@@ -7,9 +7,9 @@ import Threads from '../media/threads.svg'
 
 export default function Socials() {
     return (
-        <div style={{ padding:70 }} className="container">
+        <div className="container">
             <h2 className="lemon">LET'S CONNECT</h2>
-            <div className="hocontainer">
+            <div className="hocontainernopad">
                 <a className={Styles.socialicon} href='https://twitter.com/knotbin' target='_blank'><Image src={Twitter} width={50} height={50}></Image></a>
                 <a className={Styles.socialicon} href='https://bsky.app/profile/knotbin.xyz' target='_blank'><Image src={Bluesky} width={50} height={50}></Image></a>
                 <a className={Styles.blacksocial} href='https://github.com/knotbin' target='_blank'><Image src={Github} width={50} height={50}></Image></a>
