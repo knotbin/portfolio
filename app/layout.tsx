2baf9d729dc1ec965a664b5b3bf7a93ec4e0b52d
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-09-23 22:13:04 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './global.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
-import { GeistMono } from 'geist/font/mono'
 import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
